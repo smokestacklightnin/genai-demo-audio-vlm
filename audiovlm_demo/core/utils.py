84bb7a0419fdba8b8772195c7d2a0fe57b9b758f
smokestacklightnin <125844868+smokestacklightnin@users.noreply.github.com>
2025-02-21 18:27:30 -0800

    Add `encode_file_to_data_url`

--- audiovlm_demo/core/utils.py
+++ audiovlm_demo/core/utils.py
@@ -1,5 +1,18 @@
+import base64
+import mimetypes
 from pathlib import Path
 
 
 def resolve_path(path: Path | str) -> Path:
     return Path(path).expanduser().resolve()
+
+
+def encode_file_to_data_url(filename, file_contents) -> str:
+    """Converts image file to data url to display in browser."""
+    base64_encoded = base64.b64encode(file_contents)
+    filename_ = filename.lower()
+    mime_type = mimetypes.guess_type(filename_)[0]
+    if mime_type is None:
+        raise ValueError(f"Could not determine mimetype of file {filename_}")
+    data_url = f"data:{mime_type};base64,{base64_encoded.decode('utf-8')}"
+    return data_url
